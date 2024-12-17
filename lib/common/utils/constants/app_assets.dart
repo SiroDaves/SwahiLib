c8f656e5f12a049ecaa7fcd32e9767acb21313eb
Siro Daves <sirodevske@gmail.com>
2024-12-17 23:17:34 +0300

    remove localization

--- lib/common/utils/constants/app_assets.dart
+++ lib/common/utils/constants/app_assets.dart
@@ -6,6 +6,6 @@ class AppAssets {
   static const String dcugIcon = '$assetsImages/dcug.png';
   static const String hashtagIcon = '$assetsImages/dcug24-hashtag.png';
   static const String droidconIcon = '$assetsImages/droidcon-uganda.png';
-  static const String droidconUgIcon = '$assetsImages/droidconug.png';
+  static const String droidconUgIcon = '$assetsImages/swahilib.png';
   static const String imgZeroState = '$assetsImages/empty.png';
 }
--- lib/core/di/injectable.config.dart
+++ lib/core/di/injectable.config.dart
@@ -8,15 +8,15 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:droidconug/common/data/db/app_database.dart' as _i1040;
-import 'package:droidconug/common/repository/auth_repository.dart' as _i938;
-import 'package:droidconug/common/repository/db/database_repository.dart'
-    as _i902;
-import 'package:droidconug/common/repository/local_storage.dart' as _i1062;
-import 'package:droidconug/core/di/injectable.dart' as _i137;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 import 'package:shared_preferences/shared_preferences.dart' as _i460;
+import 'package:swahilib/common/data/db/app_database.dart' as _i853;
+import 'package:swahilib/common/repository/auth_repository.dart' as _i1013;
+import 'package:swahilib/common/repository/db/database_repository.dart'
+    as _i510;
+import 'package:swahilib/common/repository/local_storage.dart' as _i703;
+import 'package:swahilib/core/di/injectable.dart' as _i18;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -34,17 +34,17 @@ extension GetItInjectableX on _i174.GetIt {
       () => registerModule.localStorage(),
       preResolve: true,
     );
-    await gh.singletonAsync<_i1040.AppDatabase>(
+    await gh.singletonAsync<_i853.AppDatabase>(
       () => registerModule.provideAppDatabase(),
       preResolve: true,
     );
-    gh.lazySingleton<_i938.AuthRepository>(() => _i938.AuthRepository());
-    gh.lazySingleton<_i902.DatabaseRepository>(() =>
-        registerModule.provideDatabaseRepository(gh<_i1040.AppDatabase>()));
-    gh.singleton<_i1062.LocalStorage>(
-        () => _i1062.LocalStorage(gh<_i460.SharedPreferences>()));
+    gh.lazySingleton<_i1013.AuthRepository>(() => _i1013.AuthRepository());
+    gh.lazySingleton<_i510.DatabaseRepository>(() =>
+        registerModule.provideDatabaseRepository(gh<_i853.AppDatabase>()));
+    gh.singleton<_i703.LocalStorage>(
+        () => _i703.LocalStorage(gh<_i460.SharedPreferences>()));
     return this;
   }
 }
 
-class _$RegisterModule extends _i137.RegisterModule {}
+class _$RegisterModule extends _i18.RegisterModule {}
