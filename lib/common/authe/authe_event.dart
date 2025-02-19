c15a38013422e1716cff067d81a32170fab3bf01
Siro Daves <sirodevske@gmail.com>
2025-02-19 12:44:55 +0300

    renamed authe to auth

--- lib/common/authe/authe_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'authe_bloc.dart';
-
-@freezed
-sealed class AutheEvent with _$AutheEvent {
-  const factory AutheEvent.autheStatusChanged(
-      AutheStatus status) = AutheStatusChanged;
-  const factory AutheEvent.autheLogoutRequested() =
-      AutheLogoutRequested;
-}
