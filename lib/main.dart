b17b220184abdee58c1ce2a40d34437248aa500e
Siro Daves <sirodevske@gmail.com>
2025-02-28 21:33:02 +0300

    adding work manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:workmanager/workmanager.dart';
 
 import 'app.dart';
 import 'common/utils/app_util.dart';
 import 'common/utils/env/flavor_config.dart';
 import 'common/utils/env/environments.dart';
 import 'core/di/injectable.dart';
+
+@pragma('vm:entry-point')
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    logger("Background task: $task");
+    return true;
+  });
+}
 
 Future<void> main() async {
   logger('Starting app from main.dart');
@@ -29,7 +38,12 @@ Future<void> main() async {
   logger('Supabase init started: $supabaseUrl');
 
   await configureDependencies(Environments.production);
-  
+
+  Workmanager().initialize( 
+    callbackDispatcher,
+    isInDebugMode: true, 
+  );
+
   await SentryFlutter.init(
     (options) => options.dsn = sentryUrl,
     appRunner: () => runApp(
